7ecf8292356d1e11eb80c55ff9eac783218fe2e3
User <User@DESKTOP-KI9J8S8>
2022-07-20 12:11:52 +0530

    [rohit]:added yogesh file

---
+++ src/Sample1/Demo4.java
@@ -0,0 +1,18 @@
+package Sample1;
+
+public class Demo4 
+{
+	public static void main(String[] args) 
+	{
+		//print num 1 to 5
+		
+		for(int i=1; i<=5; i++) 
+		{
+			System.out.println(i);
+			
+		}
+		
+		
+	} 
+
+}
